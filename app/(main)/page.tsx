15512d04681d0f368071dd79eb2eec03972d7ca5
Snamude <stella.namude@gmail.com>
2025-08-31 01:43:04 +0300

    update files again

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
-
+//import React from 'react'
+//text-2xl
 export default function Homepage() {
-  return <div className='text-2xl'>Homepage</div>
-  
+ 
+  return <div className='text-2xl'>Homepage</div>;
+
 }
+
+
